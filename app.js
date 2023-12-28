fa374825cd17a7c111d12a40d594fc92ecd5138e
htdoan27 <huynhthiendoan27@gmail.com>
2023-12-28 18:52:47 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const contactsRouter = require("./app/routes/contact.route")
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,20 @@ app.use(express.json());
 //   res.json({ message: "welcome to contact book application." });
 // });
 
-app.use("/api/contacts", contactsRouter)
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 
 module.exports = app;
